6a2e85d817060716ff03df9332137e0fa83df17e
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-09-08 08:46:04 +0545

    created city schema

---
+++ prisma/migrations/20250908030023_created_city/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."cities" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "cities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cities_name_key" ON "public"."cities"("name");

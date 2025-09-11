14bbfeda513df55423364b209ac66b592a88a988
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-09-11 15:04:33 +0545

    some schema fixes, added created at and updated at field

---
+++ prisma/migrations/20250911091746_migrate_all/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `updated_at` to the `flights` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `seats` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."flights" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "public"."seats" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
